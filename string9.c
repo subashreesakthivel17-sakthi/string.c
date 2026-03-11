66242b9e50c827ce4f088e34b7d57e067540ef22
subashree <subashreesakthivel17@gmail.com>
2026-03-11 14:20:59 +0530

    Add files via upload

---
+++ string9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[300];
+    int i;
+    fgets(str,sizeof(str),stdin);
+    for(i=0;str[i]!='\0';i++){
+        if(str[i]>='a'&&str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    printf("%s",str);
+    return 0;
+}
