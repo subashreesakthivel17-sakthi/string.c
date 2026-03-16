1170f416a2d60cb260866c818891a2ec1704198f
subashree <subashreesakthivel17@gmail.com>
2026-03-16 11:33:18 +0530

    Add files via upload

---
+++ function1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    float simpleinterest(int p,int r,int t){
+    return(p*r*t/100.0);
+    }
+    int main(){
+        int P,R,T;
+        scanf("%d %d %d",&P,&R,&T);
+        float si=simpleinterest(P,R,T);
+        printf("%2.f",si);
+        return 0;
+        
+    }
